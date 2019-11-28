b862b1652127eaeb257c92566ae2a5e01de2742d
Luywer <luisafonsoso@gmail.com>
2019-11-28 18:27:27 +0000

    fixed small error, improved interface

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:projeto_esof/main.dart';
+import 'package:projeto_esof/userInfo/screens.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
